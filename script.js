44bb20d66a1f4f10dc83c42bf79c5d4c3a7132a3
aris_giotis <apjoti@gmail.com>
2022-08-20 18:58:03 +0300

    Use conditionals in JS, when 10 then recount..

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ add.addEventListener("click", function() {
     let output = document.querySelector("#output");
     let result = Number(output.innerText) + 1;
 
+    if (result > 10) {
+        result = 0;
+    }
+
     output.innerText = result;
 })
