40eda38de55592f5dbfc60323ca4f83e7d84170f
aris_giotis <apjoti@gmail.com>
2022-08-20 17:54:20 +0300

    Variables referencing to hmtl buttons

--- README.md
+++ README.md
@@ -4,14 +4,19 @@ Learn JavaScript Basics by Building a Counter Application
 
 # Objectives
 
-increase familiarity with simple Javascript notions & concepts
-understand how variables work in JS
-let
-const
+- increase familiarity with simple Javascript notions & concepts
+  defer
+- understand how variables work in JS
+  let
+  const
+- best practices
+  naming variables
+  avoid reserved words
+- how to use operators
 
 # Tech stack & tools
 
 JavaScript
 VS Code
 modern web browser
-Live Server (VS Code extension)
+Live Server (VS Code extension, cmd + L + O)
--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-let first_variable = "hello world" // can be modified
-const last_variable = "something" // cannot change it's value
+// note: variables can contain references to HTML elements
+// we can save the references to our buttons in variables
 
-let my_string = "Hello world"
-let my_second_string = "24"
+let add = document.querySelector("#add");
+let subtract = document.querySelector("#subtract");
 
-let my_number = 15 // number
-let my_second_number = "15" // string
-
-let my_array = [1, "hello", "4", "world"] // array
-
-let my_boolean = true // boolean
-
-let my_obj = {  // objects allow you to store data in key-value pairs
-    name: "John snow",
-    aim: "Learning JavaScript",
-    age: 20,
-}
-
-// JS interprets everything as an object
-
-let my_button = document.querySelector("#id") // you can even save references to HTML elements in variables
-
-my_button // to use a variable in JS, simply call the variable name like this
+console.log(add, subtract);
