f73c61d1761bff79e6358904d0e91d864cb0acc1
aris_giotis <apjoti@gmail.com>
2022-08-20 18:52:38 +0300

    011111 String concatenation, logic bug

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 let add = document.querySelector("#add");
-let subtract = document.querySelector("#subtract");
 
-// now that we have referenced html elements to variables
-// we need to know if these elements underwent some EVENT
-// eg if they have been clicked
+add.addEventListener("click", function() {
+    let output = document.querySelector("#output"); // -> span
+    let result = output.innerText + 1;
 
-// EVENTS are used to LISTEN to events that occur on a web page,
-// such as when a user clicks a button,
-// or scrolls down the page, among other things
-
-add.addEventListener("click", function() { // to add a click event to the button
-    // Every time the add buttin is pressed, this code will execute.
-    // Code in this scope (listener's callback) get's called
+    output.innerText = result;
 })
