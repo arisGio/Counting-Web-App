aef4fcc5c0d9a9f6cb0e3ef0db0618449719ebe2
aris_giotis <apjoti@gmail.com>
2022-08-20 19:09:40 +0300

    Code optimization, avoid code duplication, use functions

--- script.js
+++ script.js
@@ -22,3 +22,19 @@ subtract.addEventListener("click", function() {
 
     output.innerText = result;
 })
+
+// Discussion of functions
+function add() {
+    alert("hello world")
+}
+
+// function with PARAMETERS
+function add(number1, number2) { // parameters = placeholders
+    return number1 + number2
+}
+
+// functions are called by their names, like variables, but include () parenthesis
+add(2,4) // 6
+
+// ARROW FUNCTIONS
+const add = (number1, number2) => number1 + number2 // equivalent to add() defined above
