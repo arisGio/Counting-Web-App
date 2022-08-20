225df0e25297514c6c3a8a274b8db7dd71ade3c4
aris_giotis <apjoti@gmail.com>
2022-08-20 18:54:41 +0300

    Fix logic bug, Number() method

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let add = document.querySelector("#add");
 
 add.addEventListener("click", function() {
-    let output = document.querySelector("#output"); // -> span
-    let result = output.innerText + 1;
+    let output = document.querySelector("#output");
+    let result = Number(output.innerText) + 1;
 
     output.innerText = result;
 })
