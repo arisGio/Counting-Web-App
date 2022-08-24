9fd078eaecc76fd679a9435e98d0fb068dff94e0
aris_giotis <apjoti@gmail.com>
2022-08-25 00:11:02 +0300

    Added HTML onclick Event Attribute & optimized code, avoiding duplication

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
 let add = document.querySelector("#add");
 let subtract = document.querySelector("#subtract");
+let output = document.querySelector("#output");
 
-add.addEventListener("click", function() {
-    let output = document.querySelector("#output");
-    let result = Number(output.innerText) + 1;
+let count = Number(output.innerText);
 
-    if (result > 10) {
-        result = 0;
+function changeCounter(button) {
+    if (button == add) {
+        count++;
+    } else if (button == subtract) {
+        if (count > 0) {
+            count--;
+        }
     }
-
-    output.innerText = result;
-})
-
-subtract.addEventListener("click", function() {
-    let output = document.querySelector("#output");
-    let result = Number(output.innerText) - 1;
-
-    if (result < 0) {
-        result = 0;
-    }
-
-    output.innerText = result;
-})
-
-// Discussion of functions
-function add() {
-    alert("hello world")
+    output.innerText = count;
 }
-
-// function with PARAMETERS
-function add(number1, number2) { // parameters = placeholders
-    return number1 + number2
-}
-
-// functions are called by their names, like variables, but include () parenthesis
-add(2,4) // 6
-
-// ARROW FUNCTIONS
-const add = (number1, number2) => number1 + number2 // equivalent to add() defined above
